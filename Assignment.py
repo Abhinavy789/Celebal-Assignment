b4aa64717b76edb9b6eb4b17bd5aad589f384e25
Abhinavy789 <97805631+Abhinavy789@users.noreply.github.com>
2024-06-06 17:55:16 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,23 @@
+#Create lower triangular, upper triangular and pyramid containing the "*" character.
+
+def lower_triangular(n):
+    for i in range(1, n + 1):
+        print('*' * i)
+
+def upper_triangular(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+def pyramid(n):
+    for i in range(1, n + 1):
+        print(' ' * (n - i) + '*' * (2 * i - 1))
+
+
+n = int(input("Enter the value of n: "))
+
+print("Lower Triangular Pattern:")
+lower_triangular(n)
+print("\nUpper Triangular Pattern:")
+upper_triangular(n)
+print("\nPyramid Pattern:")
+pyramid(n)
